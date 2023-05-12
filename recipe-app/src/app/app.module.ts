bfaff5b0f70e993eddfb84dad6247a0a594f6035
martinm <miguel-angel.martin@avl.com>
2023-05-12 14:14:54 +0200

    new app

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RecepiesComponent } from './components/recepies/recepies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecepiesComponent
   ],
   imports: [
     BrowserModule
