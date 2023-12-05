7a480558946ec765b87493dda11e6860dfde9266
miguel <mimarben@gmail.com>
2023-12-05 08:18:58 +0100

    recipes

--- recipe-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,6 +13,15 @@ export class RecipeListComponent {
     new Recipe('1 Pizza', '1 A pizza is a sliced dough formed from bread dough.', 'https://p1.pxfuel.com/preview/943/14/377/pizza-dough-tomato-macro.jpg'),
     new Recipe('2 Pizza', '2 A pizza is a sliced dough formed from bread dough.', 'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_5-SQjpg.jpg'),
     new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg'),
+    new Recipe('3 Pizza', '3 A pizza is a sliced dough formed from bread dough.', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-1.jpg')
   ];
 
   constructor() { }
