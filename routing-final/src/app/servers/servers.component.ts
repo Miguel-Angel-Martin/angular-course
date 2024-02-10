788f2dd8a403a7339ade5984e6d17e414cfd26f6
martinm <mimarben@gmail.com>
2024-02-10 09:16:57 +0100

    end of routing

--- routing-final/src/app/servers/servers.component.ts
+++ routing-final/src/app/servers/servers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -12,12 +12,15 @@ export class ServersComponent implements OnInit {
 
   constructor(private serversService: ServersService,
               private router: Router,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
-  onReload(){
-    this.router.navigate(['servers'],{relativeTo:this.route});
+
+  onReload() {
+    // this.router.navigate(['servers'], {relativeTo: this.route});
   }
+
 }
