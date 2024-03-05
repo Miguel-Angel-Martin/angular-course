bd0fc1c752981a22ebb2c4b5a56cfc750120c42e
martinm <miguel-angel.martin@avl.com>
2024-03-05 13:24:03 +0100

    forms

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
