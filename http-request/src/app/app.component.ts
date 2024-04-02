471c6b8f74663c63183f482e90134002338ce64a
martinm <miguel.martin.benavides@gmail.com>
2024-04-02 12:01:31 +0200

    loading

--- http-request/src/app/app.component.ts
+++ http-request/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './models/post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -31,6 +32,7 @@ export class AppComponent implements OnInit {
   }
   private fetchPosts() {
     // Send Http request
+    this.isFetching = true;
     this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-f2557-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
       .pipe(map((responseData) => {
         const postArray: Post[] = [];
@@ -42,6 +44,7 @@ export class AppComponent implements OnInit {
         return postArray;
       }))    
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
         //console.log(posts);
     })
