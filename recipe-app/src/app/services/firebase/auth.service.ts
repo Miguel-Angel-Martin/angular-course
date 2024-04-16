6b75b41b255df63d99bd42a6849d32e01873e45d
martinm <mimarben@gmail.com>
2024-04-16 21:55:52 +0200

    user model

--- recipe-app/src/app/services/firebase/auth.service.ts
+++ recipe-app/src/app/services/firebase/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
@@ -21,31 +21,37 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     }).pipe(
-      catchError(errorRes => {
-        let errorMessage = 'An unknown error occurred!';
-        if (!errorRes.error || !errorRes.error.error) {
-          return;
-        }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email exists already';
-            break;
-          case 'EMAIL_NOT_FOUND':
-            errorMessage = 'This email does not exist.';
-            break;
-          case 'INVALID_PASSWORD':
-            errorMessage = 'This password is not correct.';
-            break;
-        }
-        return throwError(errorMessage);
-      })
-    );
+      catchError(this.handleError))
   }
   login(email:string, password:string){
       return this.http.post<AuthResponseData>(Constants.API_LOGIN+Constants.API_KEY, {
         email:email,
         password: password,
         returnSecureToken: true
-    })
+    }).pipe(
+      catchError(this.handleError))
+  }
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    console.log(errorRes.error.error.message);
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'This user is disabled.';
+      case 'INVALID_LOGIN_CREDENTIALS':
+        errorMessage = 'Invalid login credentials';
+    }
+    return throwError(errorMessage);
   }
 }
