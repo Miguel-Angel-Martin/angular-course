3f140a10b0b60e4a0c08ead1cfffe412e6708974
martinm <mimarben@gmail.com>
2024-05-17 17:17:20 +0200

    recipe app from trainer working with my code in angular 17

---
+++ recipe-app-course/src/app/config/constants.ts
@@ -0,0 +1,10 @@
+// Example of grouping constants in an object
+export const Constants = {
+  API_URL: 'https://recipe-app-26309-default-rtdb.europe-west1.firebasedatabase.app/',
+  API_RECIPE: 'recipes.json',
+  DEFAULT_LANGUAGE: 'en',
+  //INFO: https://firebase.google.com/docs/reference/rest/auth
+  API_SIGNUP: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+  API_LOGIN:"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=",
+  API_KEY:'AIzaSyD-N24AM6nXWi00L39NgMZRJEcTir3orx4'
+};
