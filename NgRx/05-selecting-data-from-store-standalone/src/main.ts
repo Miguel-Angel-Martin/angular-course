23ddb74223004a1b5999a4f9be35b198386457f6
martinm <miguel-angel.martin@avl.com>
2024-07-01 07:46:04 +0200

    remove standalone...it's the same both codes

--- NgRx/05-selecting-data-from-store-standalone/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideStore } from '@ngrx/store';
-
-import { AppComponent } from './app/app.component';
-import { counterReducer } from './app/store/counter.reducer';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideStore({ counter: counterReducer })],
-});
